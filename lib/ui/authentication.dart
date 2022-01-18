9f2315a2beb70813c64b3c2cc1de060e9e86d9fe
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2022-01-18 11:39:30 +0530

    Created basic authentication screen

---
+++ lib/ui/authentication.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple.shade700, Colors.indigo],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: _emailField,
+                decoration: InputDecoration(
+                  hintText: 'xyz@abcd.com',
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: 'Enter your email',
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'xxxxxxxxxx',
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: 'Enter your password',
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.cyanAccent,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text('Click to Register'),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.cyanAccent,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text('Click to Login'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
