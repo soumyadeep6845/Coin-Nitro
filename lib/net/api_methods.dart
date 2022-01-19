281ea22c3e16fc5704d61c84e08249f87a275097
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2022-01-19 21:25:19 +0530

    Fetched data from coingecko API

---
+++ lib/net/api_methods.dart
@@ -0,0 +1,17 @@
+//https://api.coingecko.com/api/v3/coins
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<double> getPrice(String id) async {
+  try {
+    Uri url = Uri.parse('https://api.coingecko.com/api/v3/coins/' + id);
+    var response = await http.get(url);
+    var json = jsonDecode(response.body);
+    var value = json['market_data']['current_price']['inr'].toString();
+    return double.parse(value);
+  } catch (e) {
+    print(e.toString());
+    return 0.0;
+  }
+}
--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto_app/net/api_methods.dart';
 import 'package:crypto_app/ui/add_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -9,8 +10,35 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  double bitcoin = 0.0;
+  double ethereum = 0.0;
+  double tether = 0.0;
+
+  @override
+  void initState() {
+    getValues();
+    super.initState();
+  }
+
+  getValues() async {
+    bitcoin = await getPrice('bitcoin');
+    ethereum = await getPrice('ethereum');
+    tether = await getPrice('tether');
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    double getValue(String id, double amount) {
+      if (id == 'bitcoin') {
+        return bitcoin * amount;
+      } else if (id == 'ethereum') {
+        return ethereum * amount;
+      } else {
+        return tether * amount;
+      }
+    }
+
     return Scaffold(
       body: Container(
         width: MediaQuery.of(context).size.width,
@@ -37,7 +65,8 @@ class _HomeViewState extends State<HomeView> {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Text('Coin Name: ${document.id}'),
-                      Text('Amount owned: ${document['Amount']}'), //CHECK HEREEEEEEEEEE
+                      Text(
+                          'Amount owned: ₹${getValue(document.id, document['Amount']).toStringAsFixed(2)}'),
                     ],
                   ),
                 );
